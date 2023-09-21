5efec4ecc41d93d0fa9abd4e6223987eb373daac
Crowdbotics <team@crowdbotics.com>
2023-09-21 20:26:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-flower-43501",
+  url: "https://late-flower-43501.botics.co",
+  api: "https://late-flower-43501.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
